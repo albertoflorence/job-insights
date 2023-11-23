76ab8f9c7784999e6ea984c79e1004bd3620b145
albertoflorence <alberto.sabatier@gmail.com>
2023-11-23 14:15:19 -0300

    lint fix

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,6 +1,7 @@
 # from src.pre_built.counter import count_ocurrences
 from src.pre_built.counter import count_ocurrences
 
+
 def test_counter():
     result = count_ocurrences('data/jobs.csv', 'python')
     assert result == 1639
