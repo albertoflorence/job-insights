f155b9d7dc498e86266c51afaad2fe99ef224b46
albertoflorence <alberto.sabatier@gmail.com>
2023-11-23 16:36:31 -0300

    requisito 10 - Read Brazilian File (Testes)

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
 # from src.pre_built.brazilian_jobs import read_brazilian_file
-
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 def test_brazilian_jobs():
-    raise
+    result = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert result[0].get('title') == 'Maquinista'
+    assert result[0].get('salary') == '2000'
+    assert result[0].get('type') == 'trainee'
