7bcb6d8e2d34d0f530dae81bbc55e642db4b804f
albertoflorence <alberto.sabatier@gmail.com>
2023-11-23 16:20:36 -0300

    requisito 8 - implement matches_salary_range method

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -19,7 +19,23 @@ class ProcessSalaries(ProcessJobs):
         return min(salaries, default=0)
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+        min_salary = job.get('min_salary')
+        max_salary = job.get('max_salary')
+
+        if min_salary is None or max_salary is None:
+            raise ValueError('salary range is not specified')
+
+        try:
+            salary = int(salary)
+            min_salary = int(min_salary)
+            max_salary = int(max_salary)
+        except (ValueError, TypeError):
+            raise ValueError('salary is not a number')
+
+        if min_salary > max_salary:
+            raise ValueError('minimum salary is greater than maximum salary')
+
+        return min_salary <= salary <= max_salary
 
     def filter_by_salary_range(
         self, jobs: List[dict], salary: Union[str, int]
